bfccdbd2df4c6dd05857fec429d3d859a92442b0
bimsant <santos.bims@gmail.com>
2022-06-20 14:18:08 -0300

    acrescentando function

--- sintaxe-operadores/operadores.js
+++ sintaxe-operadores/operadores.js
@@ -1,17 +1,29 @@
 function operadores (a, b) {
-    let igual;
-    let n10;
-    let n20;
-    const soma = a + b;
+    const numerosIguais = primeiraFrase(a, b);
+    const maiorMenor = segundaFrase(a, b);
 
-    a === b ? igual = "são iguais" : igual = "não são iguais";
+    console.log (`${numerosIguais} ${maiorMenor}`);
 
-    soma > 10 ? n10 = "maior" : n10 = "menor";
-    soma > 20 ? n20 = "maior" : n20 = "menor";
-
-    return(`Os números ${a} e ${b} ${igual}. Sua soma é ${soma}, que é ${n10} que 10 e ${n20} que 20.`)
-
+    function primeiraFrase (a, b) {
+        let igual;
+        a === b ? igual = "são iguais" : igual = "não são iguais";
+    
+        return (`Os números ${a} e ${b} ${igual}.`)
+    }
+    
+    function segundaFrase (a, b) {
+        let n10;
+        let n20;
+        const soma = a + b;
+    
+        soma > 10 ? n10 = "maior" : n10 = "menor";
+        soma > 20 ? n20 = "maior" : n20 = "menor";
+    
+        return (`Sua soma é ${soma}, que é ${n10} que 10 e ${n20} que 20.`)
+    }
 }
 
-console.log(operadores(5, 21))
+
+
+operadores(5, 13);
 
